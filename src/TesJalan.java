8b468bc34652a6fd58736d894a941f1f9d7d6388
eriana <erianaretnoputri@gmail.com>
2026-09-19 12:22:48 +0700

    American guy commit

--- src/TesJalan.java
+++ src/TesJalan.java
@@ -7,6 +7,7 @@ public class TesJalan {
 		WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println("Test Jalan");
+        System.out.println("Test Jalan Kedua");
 	}
 
 }
